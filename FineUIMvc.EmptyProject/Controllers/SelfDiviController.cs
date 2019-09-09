3d6919fa4c6e7b5192e1e4e5d01aab0283a22d1d
luis fly <askeded@163.com>
2019-09-09 15:58:12 +0800

    添加项目文件。

---
+++ FineUIMvc.EmptyProject/Controllers/SelfDiviController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FineUIMvc.EmptyProject.Controllers
+{
+    public class SelfDiviController : BaseController
+    {
+        // GET: SelfDivi
+        public ActionResult Yeah()
+        {
+            return View();
+        }
+    }
+}
---
+++ FineUIMvc.EmptyProject/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FineUIMvc.EmptyProject.Controllers
+{
+    public class TestController : BaseController
+    {
+        // GET: Test
+        public ActionResult MyTest()
+        {
+            return View();
+        }
+
+        // GET: another view
+        public ActionResult AnotherTest()
+        {
+            return View();
+        }
+
+        // 9.9 测试按钮事件控制
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult pressing()
+        {
+            Alert.Show("你好 FineUI！", MessageBoxIcon.Warning);
+
+            return UIHelper.Result();
+        }
+    }
+}
